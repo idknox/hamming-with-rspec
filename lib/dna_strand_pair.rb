753105a87746019d331f8bed5b0008eeb8db0f5d
Ian Knox <knoxid@gmail.com>
2014-06-26 09:57:18 -0600

    first solution commented out, second being used

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,35 @@
 class DnaStrandPair
 
+  def initialize(string1, string2)
+    @string1 = string1
+    @string2 = string2
+
+  end
+
+  def hamming_distance
+
+    x = 0
+    y = 0
+
+    while (y < @string1.length) && (y < @string2.length)
+
+      if @string1[y] != @string2[y]
+        x += 1
+      end
+      y += 1
+    end
+    x
+  end
+
+
+  # def hamming_distance
+  #
+  #   if (@string1.length > @string2.length) || (@string1.length == @string2.length)
+  #     len = @string2.length
+  #   elsif @string2.length > @string1.length
+  #     len = @string1.length
+  #   end
+  #
+  #   ((0...len).select {|num| @string1[num] != @string2[num]}).length
+  # end
 end
